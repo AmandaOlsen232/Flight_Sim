b78665bff7c080c0cfd75f012b8be4727c11f5f6
AmandaOlsen232 <130400469+AmandaOlsen232@users.noreply.github.com>
2025-08-28 15:38:25 -0600

    added more functions

--- Flight_Sim.f90
+++ Flight_Sim.f90
@@ -3,9 +3,10 @@ implicit none
 ! integer, parameter :: dp = selected_real_kind(16,307)
 
 contains
-!Write a function named quat_mult that accepts two arrays of length 
-!   4 representing 2 quaternions and returns the quaternion product
+
 function quat_mult(A, B) result(answer)
+    !    1.8.3 Write a function named quat_mult that accepts two arrays of length 
+    !   4 representing 2 quaternions and returns the quaternion product
     implicit none
 
     real, dimension(4), intent(in) :: A, B
@@ -34,57 +35,147 @@ function quat_mult(A, B) result(answer)
     
 end function quat_mult
 
-
-!Write a function named quat_base_to_dependent to compute a vector
-!   transformation using the Euler-Rodrigues quaternion. The function
-!   should accept an array of length three containing the three 
-!   components of a vector in the base coordinate system as well as
-!   an array of length four containing the four components of the
-!   quaternion that represent the orientation of the dependent 
-!   coordinate system. This function should return an array of length
-!   three containing the vector components in the dependent 
-!   coordinate system.
-function quat_base_to_dependent(base, E) result(v2)
-    
+function quat_base_to_dependent(v1, E) result(v2)
+    !1.8.4 Write a function named quat_base_to_dependent to compute a vector
+    !   transformation using the Euler-Rodrigues quaternion. The function
+    !   should accept an array of length three containing the three 
+    !   components of a vector in the base coordinate system as well as
+    !   an array of length four containing the four components of the
+    !   quaternion that represent the orientation of the dependent 
+    !   coordinate system. This function should return an array of length
+    !   three containing the vector components in the dependent 
+    !   coordinate system.
     implicit none
-    real, dimension(3), intent(in) :: base !array of length three containing the three components of a vector in the base coordinate system
+    real, dimension(3), intent(in) :: v1 !array of length three containing the three components of a vector in the base coordinate system
     real, dimension(4), intent(in) :: E !array of length four containing the four components of the quaternion that represent the orientation of the dependent coordinate system
-    real, dimension(4) :: e_rod, T
+    real, dimension(4) :: e_rod, e_rod_neg, T, v1_mult, v2_four
     real, dimension(3) :: v2
     real :: pi=3.1415926535897932385
-    real :: theta_deg, theta_rad, Ex, Ey, Ez, vx1, vy1, vz1, erod0, erodx, erody, erodz, T0, Tx, Ty, Tz
+    real :: theta_deg, theta_rad, Ex, Ey, Ez
 
     theta_deg = E(1)
     theta_rad = theta_deg*pi/180
     Ex = E(2)
     Ey = E(3)
     Ez = E(4)
-    
-    vx1 = base(1)
-    vy1 = base(2)
-    vz1 = base(3)
 
-    e_rod = [cos(theta_rad/2), &
+    
+    e_rod = [cos(theta_rad/2), &  !eq. 1.5.1
             Ex*sin(theta_rad/2), &
             Ey*sin(theta_rad/2), &
             Ez*sin(theta_rad/2)]
     
-    T = [-vx1*erodx - vy1*erody - vz1*erodz, &
-         vx1*erod0 + vy1*erodz - vz1*erody, &
-         -vx1*erodz + vy1*erod0 + vz1*erodz, &
-          vx1*erody - vy1*erodx + vz1*erod0]
-    
-    T0 = T(1)
-    Tx = T(2)
-    Ty = T(3)
-    Tz = T(4)
+    e_rod_neg = [e_rod(1), &
+                 -e_rod(2), &
+                 -e_rod(3), &
+                 -e_rod(4)]
 
-    v2 = [erod0*Tx - erodx*T0 - erody*Tz + erodz*Ty, &
-          erod0*Ty + erodx*Tz - erody*T0 - erodz*Tx, &
-         erod0*Tz - erodx*Ty - erody*Tx - erodz*T0]    
+    v1_mult = [0., &
+               v1(1), &
+               v1(2), &
+               v1(3)]
+   
+    T = quat_mult(v1_mult, e_rod) !eq. 1.5.6
+    v2_four = quat_mult(e_rod_neg, T) !eq. 1.5.7
+
+    v2 = [v2_four(2), &
+          v2_four(3), &
+          v2_four(4)]
 
 end function quat_base_to_dependent
 
+function quat_dependent_to_base(v2, E) result(v1)
+    !1.8.5 Write a function named quat_dependent_to_base to compute an inverse
+    !   transformation using the Euler-Rodrigues quaternion. The function should
+    !   accept an array of length three containing the three components of a 
+    !   vector in the dependent coordinate system as well as an array of length 
+    !   four containing the four components of the quaternion that represent the 
+    !   orientation of the dependent coordinate system. This function should return
+    !   an array of length three containing the vector components in the base
+    !   coordinate system.
+    implicit none
+    real, dimension(3), intent(in) :: v2
+    real, dimension(4), intent(in) :: E
+    real, dimension(3) :: v1
+
+    real :: theta_deg, theta_rad, Ex, Ey, Ez
+    real, dimension(4) :: e_rod, e_rod_neg, v2_mult, T, v1_four
+    real :: pi=3.1415926535897932385
+
+    theta_deg = E(1)
+    theta_rad = theta_deg*pi/180
+    Ex = E(2)
+    Ey = E(3)
+    Ez = E(4)
+
+    
+    e_rod = [cos(theta_rad/2), &  !eq. 1.5.1
+            Ex*sin(theta_rad/2), &
+            Ey*sin(theta_rad/2), &
+            Ez*sin(theta_rad/2)]
+
+    e_rod_neg = [e_rod(1), &
+                 -e_rod(2), &
+                 -e_rod(3), &
+                 -e_rod(4)]
+
+    v2_mult = [0., &
+               v2(1), &
+               v2(2), &
+               v2(3)]
+
+    T = quat_mult(v2_mult, e_rod_neg) !eq. 1.5.5
+    v1_four = quat_mult(e_rod, T) !eq. 1.5.5
+    v1 = [v1_four(2), &
+          v1_four(3), &
+          v1_four(4)] 
+end function quat_dependent_to_base
+
+function quat_norm(Q) result(Q_norm)
+    !1.8.6 Write a function named quat_norm that accepts an array of length four 
+    !   containing a quaternion, and returns the normalized quaternion. For this
+    !   computation, use the exact solution given in Eq. (1.5.12). You may wish to
+    !   overwrite the quaternion that was passed in to the function with the 
+    !   normalized solution.
+    implicit none
+    real, dimension(4), intent(in) :: Q
+    real, dimension(4) :: Q_norm
+    real :: mag
+
+    mag = sqrt(Q(1)*Q(1) + Q(2)*Q(2) + Q(3)*Q(3) + Q(4)*Q(4)) !eq. 1.5.12
+    Q_norm = Q/mag
+
+end function quat_norm
+
+function euler_to_quat(Euler) result(e)
+    !1.8.7 Write a function called euler_to_quat that accepts an array of length
+    !   three containing the three Euler angles and returns an array of length 
+    !   four containing values for the equivalent quaternion components.
+    implicit none
+    real, dimension(3), intent(in) :: Euler
+    real, dimension(4) :: e 
+    real :: phi_rad, theta_rad, psi_rad
+    real :: pi=3.1415926535897932385
+
+    phi_rad = Euler(1)*pi/180
+    theta_rad = Euler(2)*pi/180
+    psi_rad = Euler(3)*pi/180
+
+    e = [cos(phi_rad/2)*cos(theta_rad/2)*cos(psi_rad/2) + sin(phi_rad/2)*sin(theta_rad/2)*sin(psi_rad/2), &
+         sin(phi_rad/2)*cos(theta_rad/2)*cos(psi_rad/2) - cos(phi_rad/2)*sin(theta_rad/2)*sin(psi_rad/2), &
+         cos(phi_rad/2)*sin(theta_rad/2)*cos(psi_rad/2) + sin(phi_rad/2)*cos(theta_rad/2)*sin(psi_rad/2), &
+         cos(phi_rad/2)*cos(theta_rad/2)*sin(psi_rad/2) - sin(phi_rad/2)*sin(theta_rad/2)*cos(psi_rad/2)]
+
+end function euler_to_quat
+
+! function quat_to_euler() result()
+!     !1.8.8 Write a function named quat_to_euler that accepts an array of length 
+!     !   four containing the four components of a quaternion and returns an array 
+!     !   of length three containing values for the equivalent Euler angles.
+!     implicit none
+
+
+! end function quat_to_euler
 
 end module my_functions
 
@@ -92,8 +183,8 @@ program Flight_Sim
 use my_functions
 
 implicit none
-real, dimension(3) :: test
+real, dimension(4) :: test
 
-test = quat_base_to_dependent([1., 2., 3.], [45., 2., 3., 4.])
+test = euler_to_quat([45., 45., 45.])
 write(*,*) test
 end program Flight_Sim
