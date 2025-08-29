23b967dab3fe7ab3f777bee77d2b4a171d05c836
Amanda Olsen <amandaolsen169@gmail.com>
2025-08-29 11:17:23 -0600

    made some changes to functions

--- Flight_Sim.f90
+++ Flight_Sim.f90
@@ -1,6 +1,7 @@
 module my_functions
 implicit none
 ! integer, parameter :: dp = selected_real_kind(16,307)
+real :: pi=3.1415926535897932385
 
 contains
 
@@ -35,7 +36,7 @@ function quat_mult(A, B) result(answer)
     
 end function quat_mult
 
-function quat_base_to_dependent(v1, E) result(v2)
+function quat_base_to_dependent(v1, e) result(v2)
     !1.8.4 Write a function named quat_base_to_dependent to compute a vector
     !   transformation using the Euler-Rodrigues quaternion. The function
     !   should accept an array of length three containing the three 
@@ -47,36 +48,22 @@ function quat_base_to_dependent(v1, E) result(v2)
     !   coordinate system.
     implicit none
     real, dimension(3), intent(in) :: v1 !array of length three containing the three components of a vector in the base coordinate system
-    real, dimension(4), intent(in) :: E !array of length four containing the four components of the quaternion that represent the orientation of the dependent coordinate system
-    real, dimension(4) :: e_rod, e_rod_neg, T, v1_mult, v2_four
+    real, dimension(4), intent(in) :: e !array of length four containing the four components of the quaternion that represent the orientation of the dependent coordinate system
+    real, dimension(4) :: e_neg, T, v1_mult, v2_four
     real, dimension(3) :: v2
-    real :: pi=3.1415926535897932385
-    real :: theta_deg, theta_rad, Ex, Ey, Ez
 
-    theta_deg = E(1)
-    theta_rad = theta_deg*pi/180
-    Ex = E(2)
-    Ey = E(3)
-    Ez = E(4)
-
-    
-    e_rod = [cos(theta_rad/2), &  !eq. 1.5.1
-            Ex*sin(theta_rad/2), &
-            Ey*sin(theta_rad/2), &
-            Ez*sin(theta_rad/2)]
-    
-    e_rod_neg = [e_rod(1), &
-                 -e_rod(2), &
-                 -e_rod(3), &
-                 -e_rod(4)]
+    e_neg = [e(1), &
+                 -e(2), &
+                 -e(3), &
+                 -e(4)]
 
     v1_mult = [0., &
                v1(1), &
                v1(2), &
                v1(3)]
    
-    T = quat_mult(v1_mult, e_rod) !eq. 1.5.6
-    v2_four = quat_mult(e_rod_neg, T) !eq. 1.5.7
+    T = quat_mult(v1_mult, e) !eq. 1.5.6
+    v2_four = quat_mult(e_neg, T) !eq. 1.5.7
 
     v2 = [v2_four(2), &
           v2_four(3), &
@@ -84,7 +71,7 @@ function quat_base_to_dependent(v1, E) result(v2)
 
 end function quat_base_to_dependent
 
-function quat_dependent_to_base(v2, E) result(v1)
+function quat_dependent_to_base(v2, e) result(v1)
     !1.8.5 Write a function named quat_dependent_to_base to compute an inverse
     !   transformation using the Euler-Rodrigues quaternion. The function should
     !   accept an array of length three containing the three components of a 
@@ -95,37 +82,22 @@ function quat_dependent_to_base(v2, E) result(v1)
     !   coordinate system.
     implicit none
     real, dimension(3), intent(in) :: v2
-    real, dimension(4), intent(in) :: E
+    real, dimension(4), intent(in) :: e
+    real, dimension(4) :: e_neg, T, v2_mult, v1_four
     real, dimension(3) :: v1
 
-    real :: theta_deg, theta_rad, Ex, Ey, Ez
-    real, dimension(4) :: e_rod, e_rod_neg, v2_mult, T, v1_four
-    real :: pi=3.1415926535897932385
-
-    theta_deg = E(1)
-    theta_rad = theta_deg*pi/180
-    Ex = E(2)
-    Ey = E(3)
-    Ez = E(4)
-
-    
-    e_rod = [cos(theta_rad/2), &  !eq. 1.5.1
-            Ex*sin(theta_rad/2), &
-            Ey*sin(theta_rad/2), &
-            Ez*sin(theta_rad/2)]
-
-    e_rod_neg = [e_rod(1), &
-                 -e_rod(2), &
-                 -e_rod(3), &
-                 -e_rod(4)]
+    e_neg = [e(1), &
+                 -e(2), &
+                 -e(3), &
+                 -e(4)]
 
     v2_mult = [0., &
                v2(1), &
                v2(2), &
                v2(3)]
 
-    T = quat_mult(v2_mult, e_rod_neg) !eq. 1.5.5
-    v1_four = quat_mult(e_rod, T) !eq. 1.5.5
+    T = quat_mult(v2_mult, e_neg) !eq. 1.5.5
+    v1_four = quat_mult(e, T) !eq. 1.5.5
     v1 = [v1_four(2), &
           v1_four(3), &
           v1_four(4)] 
@@ -147,15 +119,14 @@ function quat_norm(Q) result(Q_norm)
 
 end function quat_norm
 
-function euler_to_quat(Euler) result(e)
+function euler_to_quat(euler) result(e)
     !1.8.7 Write a function called euler_to_quat that accepts an array of length
     !   three containing the three Euler angles and returns an array of length 
     !   four containing values for the equivalent quaternion components.
     implicit none
-    real, dimension(3), intent(in) :: Euler
+    real, dimension(3), intent(in) :: euler
     real, dimension(4) :: e 
     real :: phi_rad, theta_rad, psi_rad
-    real :: pi=3.1415926535897932385
 
     phi_rad = Euler(1)*pi/180
     theta_rad = Euler(2)*pi/180
@@ -168,14 +139,39 @@ function euler_to_quat(Euler) result(e)
 
 end function euler_to_quat
 
-! function quat_to_euler() result()
-!     !1.8.8 Write a function named quat_to_euler that accepts an array of length 
-!     !   four containing the four components of a quaternion and returns an array 
-!     !   of length three containing values for the equivalent Euler angles.
-!     implicit none
+function quat_to_euler(e) result(euler)
+    !1.8.8 Write a function named quat_to_euler that accepts an array of length 
+    !   four containing the four components of a quaternion and returns an array 
+    !   of length three containing values for the equivalent Euler angles.
+    implicit none
+    real, dimension(4) :: e
+    real, dimension(3) :: euler
+    real :: e0, ex, ey, ez, psi
 
+    e0 = e(1)
+    ex = e(2)
+    ey = e(3)
+    ez = e(4)
+    psi = 0.
 
-! end function quat_to_euler
+    write(*,*) e0*ey - ex*ez
+    if (e0*ey - ex*ez == 0.5) then
+        euler = [2*asin(ex/cos(pi/4)) + psi, &
+                 pi/2, &
+                 psi]
+    end if
+
+    if (e0*ey - ex*ez == -0.5) then
+        euler = [2*asin(ex/cos(pi/4)) - psi, &
+                 -pi/2, &
+                 psi]
+    else
+    euler = [atan2(2*(e0*ex + ey*ez), (e0*e0 + ez*ez - ex*ex - ey*ey)), &
+             asin(2*(e0*ey - ex*ez)), &
+             atan2(2*(e0*ez + ex*ey), (e0*e0 + ex*ex - ey*ey - ez*ez))]
+    end if
+
+end function quat_to_euler
 
 end module my_functions
 
@@ -183,8 +179,8 @@ program Flight_Sim
 use my_functions
 
 implicit none
-real, dimension(4) :: test
+real, dimension(3) :: test
 
-test = euler_to_quat([45., 45., 45.])
+test = quat_dependent_to_base([0., 1., 0.], [1., 2., 3., 4.])
 write(*,*) test
 end program Flight_Sim
