9de617856bb0620fd3536be578fd6dce51d0db81
Amanda Olsen <amandaolsen169@gmail.com>
2025-09-17 22:50:12 -0600

    hello

--- olsen.f90
+++ olsen.f90
@@ -340,112 +340,167 @@ subroutine vary_alt_english(Z, T, P, rho, a)
     end do
 end subroutine vary_alt_english 
 
-! function runge_kutta(t0, y0, del_t) result(y1)
-!     !to = time at begining of step
-!     !y0 = function value at beginning of step
-!     !del_t = step size 
-!     !y = function value at end of step
-!     implicit none 
-!     real, intent(in) :: t0, y0, del_t 
-!     real :: y1
-!     real :: k1, k2, k3, k4 
-
-!     k1 = differential_equations(t0, y0)
-!     k2 = differential_equations(t0 + del_t/2, y0 + k1*del_t/2)
-!     k3 = differential_equations(t0 + del_t/2, y0 + k2*del_t/2)
-!     k4 = differential_equations(t0 + del_t, y0 + k3*del_t)
-    
-!     y1 = y0 + (del_t/6)*(k1 + 2*k2 + 2*k3 + k4)
-!     write(*,*) t0, y0, k1, k2, k3, k4, y1
-! end function runge_kutta
-
-! function differential_equations(t, y) result(dy_dt)
-!     !t = time 
-!     !y = value of y at time t 
-!     !dy_dt = change in y with respecdt to t 
-!     implicit none 
-!     real, intent(in) :: t, y
-!     real :: dy_dt 
-
-!     dy_dt = 1 + tan(y)
-! end function differential_equations
-
-! subroutine test_main(t0, y0, del_t, y1, it) 
-!     implicit none 
-!     integer, intent(in) :: it
-!     integer :: i 
-!     real :: t0, y0, del_t
-!     real, intent(inout) :: y1
-
-!     do i=1, it
-!         y1 = runge_kutta(t0, y0, del_t)
-!         t0 = t0 + del_t 
-!         y0 = y1 
-!     end do
-! end subroutine test_main 
-
+!_____________________________________________________________________________________________
 function runge_kutta(t0, y0, del_t) result(y1)
-    !to = time at begining of step
-    !y0 = function value at beginning of step
-    !del_t = step size 
-    !y = function value at end of step
-    implicit none 
+    implicit none
     real, intent(in) :: t0, del_t
     real, dimension(:), intent(in) :: y0
     real, dimension(:), allocatable :: y1, k1, k2, k3, k4
-
     k1 = differential_equations(t0, y0)
     k2 = differential_equations(t0 + del_t/2, y0 + k1*del_t/2)
     k3 = differential_equations(t0 + del_t/2, y0 + k2*del_t/2)
     k4 = differential_equations(t0 + del_t, y0 + k3*del_t)
-    
     y1 = y0 + (del_t/6)*(k1 + 2*k2 + 2*k3 + k4)
-    write(*,*) t0, y0
+
 end function runge_kutta
 
-function differential_equations(t, y) result(dy_dt)
-    !t = time 
-    !y = value of y at time t 
-    !dy_dt = change in y with respecdt to t 
+subroutine mass_inertia(t, y, M, I)
     implicit none 
     real, intent(in) :: t
-    real, dimension(:), intent(in) :: y
-    real, dimension(:), allocatable :: dy_dt
-    real :: x, z 
+    real, dimension(:), intent(in) :: y 
+    real, intent(inout) :: M 
+    real, dimension(:,:), intent(inout), allocatable :: I
 
-    x = y(1)
-    z = y(2) 
-    dy_dt = [t + z**2*sin(x), t + x*cos(z)]
+    allocate(I(3,3))
+    M = 1.864857010294e-04 
+    I = 5.215657263284e-07 * reshape([1.0, 0.0, 0.0, &
+                                      0.0, 1.0, 0.0, &
+                                      0.0, 0.0, 1.0], shape=[3,3])
+end subroutine mass_inertia
 
+subroutine pseudo_aerodynamics(t, y, Fb, Mb)
+    implicit none 
+    real, intent(in) :: t 
+    real, dimension(:), intent(in) :: y 
+    real, dimension(3), intent(inout) :: Fb, Mb 
+
+    Mb = [0.0044494277934, -7.795491894285, 5.195511453592]
+    Fb = [-2.854553702798, -0.5135959520035, -0.7681692141086]
+
+end subroutine pseudo_aerodynamics
+
+function inv_3d(A) result(A_inv)
+    implicit none 
+    real, dimension(3,3), intent(in) :: A
+    real, dimension(3,3) :: A_inv, A_tilde 
+    real :: det_A  
+    det_A = A(1,1)*A(2,2)*A(3,3) + A(1,2)*A(2,3)*A(3,1) + A(1,3)*A(2,1)*A(3,2) &
+            - A(1,3)*A(2,2)*A(3,1) - A(1,2)*A(2,1)*A(3,3) - A(1,1)*A(2,3)*A(3,2)
+
+    A_tilde = reshape([A(2,2)*A(3,3)-A(2,3)*A(3,2), A(1,3)*A(3,2)-A(1,2)*A(3,3), A(1,2)*A(2,3)-A(1,3)*A(2,2), &
+                       A(2,3)*A(3,1)-A(2,1)*A(3,3), A(1,1)*A(3,3)-A(1,3)*A(3,1), A(1,3)*A(2,1)-A(1,1)*A(2,3), &
+                       A(2,1)*A(3,2)-A(2,2)*A(3,1), A(1,2)*A(3,1)-A(1,1)*A(3,2), A(1,1)*A(2,2)-A(1,2)*A(2,1)], shape=[3,3])
+
+    A_inv = A_tilde/det_A
+end function inv_3d
+
+function matmul_3d_vec(A,B) result(C)
+    implicit none 
+    real, dimension(3,3), intent(in) :: A
+    real, dimension(3), intent(in) :: B
+    real, dimension(3) :: C 
+
+    C = [A(1,1)*B(1) + A(1,2)*B(2) + A(1,3)*B(3), &
+         A(2,1)*B(1) + A(2,2)*B(2) + A(2,3)*B(3), &
+         A(3,1)*B(1) + A(3,2)*B(2) + A(3,3)*B(3)]
+end function matmul_3d_vec
+
+function differential_equations(t, y_array) result(dy_dt)
+    implicit none
+    real, intent(in) :: t
+    real, dimension(:), intent(in) :: y_array
+    real, dimension(:) :: dy_dt(13), one(3), two(3), three(4), four(4)
+    real :: g, Mass, u, v, w, p, q, r, x, y, z, eo, ex, ey, ez, Fxb, Fyb, Fzb, Ixxb, Ixyb, Ixzb, Iyyb, Iyzb, Izzb, Mxb, Myb, Mzb
+    real, dimension(:,:), allocatable :: I
+    real, dimension(3) :: F, M
+
+    !y_array = [u, v, w, p, q, r, x, y, z, eo, ex, ey, ez]
+
+    !uncomment for 5.9.4
+    call pseudo_aerodynamics(t, y_array, F, M)
+    call mass_inertia(t, y_array, Mass, I)
+    g = gravity_English(0.)
+
+    Fxb = F(1)
+    Fyb = F(2) 
+    Fzb = F(3) 
+    Mxb = M(1)
+    Myb = M(2)
+    Mzb = M(3) 
+
+    Ixxb = I(1,1)
+    Ixyb = I(2,1)
+    Ixzb = I(3,1)
+    Iyyb = I(2,2)
+    Iyzb = I(3,2)
+    Izzb = I(3,3)
+    u = y_array(1) 
+    v = y_array(2)
+    w = y_array(3)
+    p = y_array(4)
+    q = y_array(5)
+    r = y_array(6)
+    x = y_array(7)
+    y = y_array(8)
+    z = y_array(9)
+    eo = y_array(10)
+    ex = y_array(11)
+    ey = y_array(12)
+    ez= y_array(13)
+    
+    one = [(1/Mass)*Fxb + g*2*(ex*ez - ey*eo) + (r*v - q*w), &
+             (1/Mass)*Fyb + g*2*(ey*ez + ex*eo) + (p*w - r*u), &
+             (1/Mass)*Fzb + g*(ez**2 + eo**2 - ex**2 - ey**2) + (q*u - p*v)]
+
+    two = [Mxb + (Iyyb - Izzb)*q*r + Iyzb*(q**2 - r**2) + Ixzb*p*q - Ixyb*p*r, &
+           Myb + (Izzb - Ixxb)*p*r + Ixzb*(r**2 - p**2) + Ixyb*q*r - Iyzb*p*q, &
+           Mzb + (Ixxb - Iyyb)*p*q + Ixyb*(p**2 - q**2) + Iyzb*p*r - Ixzb*q*r]
+    two = matmul_3d_vec(inv_3d(I), two)
+
+    three = quat_mult([eo, ex, ey, ez], quat_mult([0., u, v, w], [eo, -ex, -ey, -ez])) !add wind vec 
+
+    four = 0.5*[-ex*p -ey*q -ez*r, &
+                eo*p -ez*q +ey*r, &
+                ez*p +eo*q -ex*r, &
+                -ey*p +ex*q +eo*r]
+
+    dy_dt = [one(1), one(2), one(3), two(1), two(2), two(3), three(2), three(3), three(4), four(1), four(2), four(3), four(4)]
 end function differential_equations
 
-! subroutine test_main(t0, y0, del_t, y1, it) 
-!     implicit none 
-!     integer, intent(in) :: it
-!     integer :: i 
-!     real :: t0, y0, del_t
-!     real, intent(inout) :: y1
 
-!     do i=1, it
-!         y1 = runge_kutta(t0, y0, del_t)
-!         t0 = t0 + del_t 
-!         y0 = y1 
-!     end do
-! end subroutine test_main 
+
+function simulation_main(t0, tf, del_t, y0) result(y1)
+    implicit none 
+    real :: t0
+    real, intent(in) :: tf, del_t
+    real, dimension(:) :: y0
+    real, dimension(:), allocatable :: y1
+
+    do while (t0 < tf)
+        y1 = runge_kutta(t0, y0, del_t)
+        call quat_norm(y1(10:13))
+        t0 = t0 + del_t
+        y0 = y1
+        
+    end do
+end function simulation_main
 
 end module olsen_m
+
 
 program main
 use olsen_m
 
-real :: t0, del_t
-real, dimension(2) :: y0, dy_dt
 
+real :: t0, tf, del_t 
+real, dimension(13) :: y0, test 
+
+!y_array = [u, v, w, p, q, r, x, y, z, eo, ex, ey, ez]
 t0 = 0.0
-y0 = [0, 0]
-del_t = 0.025
-dy_dt = runge_kutta(t0, y0, del_t)
-
-write(*,*) dy_dt
+tf = 10.0 
+del_t = 0.1
+y0 = [50., 0., 0., 0., 0., 0., 0., 0., -20., 1., 0., 0., 0.]
+test = simulation_main(t0, tf, del_t, y0)
+write(*,*) test 
 
 end program main
